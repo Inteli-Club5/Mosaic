e9bd42a311b6e38b64317a9976671cf363383c4c
Victor Garcia <victor.santos@sou.inteli.edu.br>
2025-07-05 11:22:56 +0200

    feat: addition of frontend setup

---
+++ src/frontend/client/src/components/LandingPage.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'; 
+
+const LandingPage = () => {
+
+    return (
+        <div>
+            <h3>oi</h3>
+        </div>
+    );
+};
+
+export default LandingPage;
